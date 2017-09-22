f441215bdea4441d6f78ef708ce64259905437b6
@mdawg85 <margaretshelaghmcgovern@gmail.com>
2017-09-22 09:41:29 -0700

    add equal and all methods

--- lib/project.rb
+++ lib/project.rb
@@ -11,4 +11,19 @@ class Project
     saved_project = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
     @id = saved_project.first().fetch("id").to_i()
   end
-end
+
+  def ==(another_project)
+    self.title().==(another_project.title()).&(self.id().==(another_project.id()))
+  end
+
+  def self.all
+    returned_projects = DB.exec("SELECT * from projects;")
+    projects = []
+    returned_projects.each() do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
+  end
+end #Project class
